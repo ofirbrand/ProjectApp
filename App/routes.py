39a0f153c6909186bf817695f60e075a6bd93f46
ofirbrand <ofirbrand@gmail.com>
2022-12-14 23:48:39 +0200

    basic routes created

---
+++ App/routes.py
@@ -0,0 +1,27 @@
+from app_main import app
+from flask import render_template
+
+@app.route('/')
+@app.route('/welcome')
+def welcome():
+    return render_template('welcome.html')
+
+@app.route('/register_librarian')
+def register_librarian():
+    return render_template('register_librarian.html')
+
+@app.route('/register_user')
+def register_user():
+    return render_template('register_user.html')
+
+@app.route('/homepage_librarian')
+def homepage_librarian():
+    return render_template('homepage_librarian.html')
+
+@app.route('/homepage_user')
+def homepage_user():
+    return render_template('homepage_user.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
