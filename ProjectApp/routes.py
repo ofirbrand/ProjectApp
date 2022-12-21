c6606c87acb84c20fa0324f4374ff82ebe39090d
ofirbrand <ofirbrand@gmail.com>
2022-12-21 21:56:44 +0200

    create test version of reader_register page - html, route and logic

--- ProjectApp/routes.py
+++ ProjectApp/routes.py
@@ -1,8 +1,9 @@
-from flask import render_template, url_for
+from flask import render_template, url_for, request
 from ProjectApp import app
+import ProjectApp.entities as en
 
-@app.route('/')
-@app.route('/welcome')
+@app.route('/', methods=['POST', 'GET'])
+@app.route('/welcome', methods=['POST', 'GET'])
 def welcome():
     return render_template('welcome.html')
 
@@ -10,18 +11,23 @@ def welcome():
 def register_librarian():
     return render_template('register_librarian.html')
 
-@app.route('/register_user')
-def register_user():
-    return render_template('register_user.html')
+@app.route('/registerReader', methods=['POST', 'GET'])
+def registerReader():
+    if request.method == 'POST':
+        reader = en.Reader()
+        reader.email = str(request.form['email'])
+        reader.name = str(request.form['name'])
+        reader.phone_num = int(request.form['phone'])
+        reader.address = str(request.form['address'])
+        reader.password = int(request.form['password'])
+        return render_template('homepageReader.html', reader=reader)  # create homepageReader.html to show readers details
+    else:
+        return render_template('registerReader.html')
 
 @app.route('/homepage_librarian')
 def homepage_librarian():
     return render_template('homepage_librarian.html')
 
-@app.route('/homepage_user')
-def homepage_user():
-    return render_template('homepage_user.html')
-
-@app.route('/login')
-def login():
-    return render_template('login.html')
+@app.route('/homepageReader')
+def homepageReader():
+    return render_template('homepageReader.html')
