02258fc0b22e96a613f813d5b914bb984057ae9f
ofirbrand <ofirbrand@gmail.com>
2022-12-28 00:17:18 +0200

    create database connection

--- ProjectApp/__init__.py
+++ ProjectApp/__init__.py
@@ -1,9 +1,17 @@
 from flask import Flask
-# from flaskext.mysql import MySQL
+from flaskext.mysql import MySQL
 
 
 app = Flask(__name__)
-
+mysql = MySQL()
+app.config['SECRET_KEY'] = '5479437c16e750c4c8e3a432fe891395'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'Shlomo1995'
+app.config['MYSQL_DATABASE_DB'] = 'ProjectLibraries'
+mysql.init_app(app)
+connection = mysql.connect()
+cursor = connection.cursor()
 from ProjectApp import routes
 
 
--- ProjectApp/routes.py
+++ ProjectApp/routes.py
@@ -1,7 +1,7 @@
-from flask import render_template, url_for, request
+from flask import render_template, url_for, request, flash, redirect
 from ProjectApp import app
 import ProjectApp.entities as en
-# from ProjectApp import connection, cursor  # importing the functions that deals with SQL from __init__ page
+from ProjectApp import connection, cursor
 
 @app.route('/', methods=['POST', 'GET'])
 @app.route('/welcome', methods=['POST', 'GET'])
@@ -13,13 +13,19 @@ def registerLibrarian():
     if request.method == 'POST':
         email = str(request.form['email'])
         name = str(request.form['name'])
-        phone_num = int(request.form['phone'])
+        phone_num = str(request.form['phone'])
         address = str(request.form['address'])
         password = int(request.form['password'])
         begin_work_date = request.form['begin_work_date']
         branch_name = request.form['branch_name']
         librarian = en.Librarian(email, name, phone_num, address, password, begin_work_date, branch_name)
         properties = librarian.__dict__.items()
+        flash("Librarian Created Successfully", 'success')  # check why the flash pop isn't raising
+        cursor.execute("INSERT INTO Librarian(phone_number, librarian_email, full_name, librarian_password, "
+                       "begin_work_date, branch_name) VALUES(%s, %s, %s, %s, %s, %s)",
+                       (phone_num, email, name, password, begin_work_date, branch_name))
+        connection.commit()
+        # insert address to the DataBase
         return render_template('homepageLibrarian.html', librarian=librarian, properties=properties)
     else:
         title = "Librarian Register"
@@ -41,10 +47,12 @@ def registerReader():
         title = "Reader Register"
         return render_template('registerReader.html', title=title)
 
+# create librarian home page and delete the example in librarian register view function
 @app.route('/homepageLibrarian')
-def homepageLibrarian():
+def homepageLibrarian():  # create librarian home page
     return render_template('homepageLibrarian.html')
 
+# create reader home page and delete the example in reader register view function
 @app.route('/homepageReader', methods=['GET', 'POST'])
 def homepageReader():
     return render_template('homepageReader.html')
