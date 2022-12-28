3c9174d84a725fc24ca0dd9bf40e9747513232e5
ofirbrand <ofirbrand@gmail.com>
2022-12-28 15:33:21 +0200

    create database connection

--- ProjectApp/__init__.py
+++ ProjectApp/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flaskext.mysql import MySQL
+from flask_session import Session
 
 
 app = Flask(__name__)
@@ -9,6 +10,9 @@ app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'Shlomo1995'
 app.config['MYSQL_DATABASE_DB'] = 'ProjectLibraries'
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 mysql.init_app(app)
 connection = mysql.connect()
 cursor = connection.cursor()
--- ProjectApp/routes.py
+++ ProjectApp/routes.py
@@ -1,35 +1,47 @@
-from flask import render_template, url_for, request, flash, redirect
+from flask import render_template, url_for, request, flash, redirect, session
 from ProjectApp import app
 import ProjectApp.entities as en
-from ProjectApp import connection, cursor
+from ProjectApp import connection, cursor, Session
+
 
 @app.route('/', methods=['POST', 'GET'])
 @app.route('/welcome', methods=['POST', 'GET'])
 def welcome():
     return render_template('welcome.html')
 
+
 @app.route('/registerLibrarian', methods=['GET', 'POST'])
 def registerLibrarian():
     if request.method == 'POST':
         email = str(request.form['email'])
+        # add test to check if the email address is unique
         name = str(request.form['name'])
         phone_num = str(request.form['phone'])
-        address = str(request.form['address'])
+        city = str(request.form['city'])
+        street = str(request.form['street'])
+        house_num = int(request.form['house'])
         password = int(request.form['password'])
         begin_work_date = request.form['begin_work_date']
         branch_name = request.form['branch_name']
-        librarian = en.Librarian(email, name, phone_num, address, password, begin_work_date, branch_name)
-        properties = librarian.__dict__.items()
-        flash("Librarian Created Successfully", 'success')  # check why the flash pop isn't raising
+        # if there will be any necessary for librarian python class implementation
+        # address = city + " " + str(street) + " " + str(house_num)
+        # librarian = en.Librarian(email, name, phone_num, address, password, begin_work_date, branch_name)
+        # insert data to Librarian table
         cursor.execute("INSERT INTO Librarian(phone_number, librarian_email, full_name, librarian_password, "
                        "begin_work_date, branch_name) VALUES(%s, %s, %s, %s, %s, %s)",
                        (phone_num, email, name, password, begin_work_date, branch_name))
         connection.commit()
-        # insert address to the DataBase
-        return render_template('homepageLibrarian.html', librarian=librarian, properties=properties)
+        # insert data to Librarian_address table
+        cursor.execute("INSERT INTO Librarian_address(city, street, house_number, librarian_email) "
+                       "VALUES(%s, %s, %s, %s)",
+                       (city, street, house_num, email))
+        connection.commit()
+        flash(f"Librarian User Created Successfully for {name}. You Are Now Able To Login", 'success')
+        return redirect('/')
     else:
         title = "Librarian Register"
         return render_template('registerLibrarian.html', title=title)
+
 
 @app.route('/registerReader', methods=['POST', 'GET'])
 def registerReader():
@@ -37,20 +49,37 @@ def registerReader():
         email = str(request.form['email'])
         name = str(request.form['name'])
         phone_num = int(request.form['phone'])
-        address = str(request.form['address'])
+        city = str(request.form['city'])
+        street = str(request.form['street'])
+        house_num = int(request.form['house'])
         password = int(request.form['password'])
         date = request.form['date']
-        reader = en.Reader(email, name, phone_num, address, password, date)
-        items = reader.__dict__.items()
-        return render_template('homepageReader.html', reader=reader, items=items)
+        # if there will be any necessary for reader python class implementation
+        # address = city + " " + str(street) + " " + str(house_num)
+        # reader = en.Reader(email, name, phone_num, address, password, date)
+        # insert data to Librarian table
+        cursor.execute("INSERT INTO Reader(phone_number, reader_email, full_name, reader_password, "
+                       "date_of_birth) VALUES(%s, %s, %s, %s, %s)",
+                       (phone_num, email, name, password, date))
+        connection.commit()
+        # insert data to Librarian_address table
+        cursor.execute("INSERT INTO Reader_address(city, street, house_number, reader_email) "
+                       "VALUES(%s, %s, %s, %s)",
+                       (city, street, house_num, email))
+        connection.commit()
+        flash(f"Reader User Created Successfully for {name}. You Are Now Able To Login", 'success')
+        return redirect('/')
     else:
         title = "Reader Register"
+        # add query that raise the Branches names to implement into the librarian register form
         return render_template('registerReader.html', title=title)
+
 
 # create librarian home page and delete the example in librarian register view function
 @app.route('/homepageLibrarian')
 def homepageLibrarian():  # create librarian home page
     return render_template('homepageLibrarian.html')
+
 
 # create reader home page and delete the example in reader register view function
 @app.route('/homepageReader', methods=['GET', 'POST'])
