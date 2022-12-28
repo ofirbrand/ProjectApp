a41d497d045cf476bc898a42cc17ac8d8e9e495e
ofirbrand <120312808+ofirbrand@users.noreply.github.com>
2022-12-28 18:37:00 +0200

    SQL database

---
+++ ProjectLibrary.sql
@@ -0,0 +1,115 @@
+CREATE DATABASE IF NOT EXISTS `ProjectLibraries`;
+
+USE ProjectLibraries;
+
+CREATE TABLE Reader
+(
+phone_number INT NOT NULL,
+reader_email VARCHAR(50) NOT NULL,
+full_name VARCHAR(50) NOT NULL,
+reader_password VARCHAR(50) NOT NULL,
+date_of_birth DATE NOT NULL,
+	PRIMARY KEY (reader_email)
+);
+
+CREATE TABLE Branch
+(
+  branch_name VARCHAR(30) NOT NULL,
+  phone_number VARCHAR(15) NOT NULL,
+  PRIMARY KEY (branch_name)
+);
+
+insert into Branch(branch_name, phone_number) values
+('Ramat Aviv', '097496104'),
+('City Center', '097496667'),
+('Kerem Hateimanim', '097696110')
+;
+
+CREATE TABLE Librarian
+(
+  phone_number INT NOT NULL,
+  librarian_email VARCHAR(50) NOT NULL,
+  full_name VARCHAR(50) NOT NULL,
+  librarian_password VARCHAR(50) NOT NULL,
+  begin_work_date DATE NOT NULL,
+  branch_name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (librarian_email),
+  FOREIGN KEY (branch_name) REFERENCES Branch(branch_name)
+);
+
+CREATE TABLE book
+(
+  book_name VARCHAR(100) NOT NULL,
+  author VARCHAR(100) NOT NULL,
+  publisher VARCHAR(100) NOT NULL,
+  publish_year DATE NOT NULL,
+  book_id INT NOT NULL,
+  PRIMARY KEY (book_id)
+);
+
+CREATE TABLE Reader_address
+(
+  city VARCHAR(50) NOT NULL,
+  street VARCHAR(50) NOT NULL,
+  house_number INT NOT NULL,
+  reader_email VARCHAR(50) NOT NULL,
+  PRIMARY KEY (city, street, house_number, reader_email),
+  FOREIGN KEY (reader_email) REFERENCES Reader(reader_email)
+);
+
+CREATE TABLE Librarian_address
+(
+  city VARCHAR(50) NOT NULL,
+  street VARCHAR(50) NOT NULL,
+  house_number INT NOT NULL,
+  librarian_email VARCHAR(50) NOT NULL,
+  PRIMARY KEY (city, street, house_number, librarian_email),
+  FOREIGN KEY (librarian_email) REFERENCES Librarian(librarian_email)
+);
+
+CREATE TABLE Branch_address
+(
+  city VARCHAR(50) NOT NULL,
+  street VARCHAR(50) NOT NULL,
+  house_number INT NOT NULL,
+  branch_name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (city, street, house_number, branch_name),
+  FOREIGN KEY (branch_name) REFERENCES Branch(branch_name)
+);
+
+CREATE TABLE Borrow_Extension
+(
+  date_of_borrowing DATE NOT NULL,
+  request_id INT NOT NULL,
+  status_of_request VARCHAR(20) NOT NULL,
+  request_type VARCHAR(20) NOT NULL,
+  book_id INT NOT NULL,
+  reader_email VARCHAR(50) NOT NULL,
+  librarian_email VARCHAR(50) NOT NULL,
+  PRIMARY KEY (request_id),
+  FOREIGN KEY (book_id) REFERENCES book(book_id),
+  FOREIGN KEY (reader_email) REFERENCES Reader(reader_email),
+  FOREIGN KEY (librarian_email) REFERENCES Librarian(librarian_email)
+);
+
+CREATE TABLE Copies
+(
+  amount INT NOT NULL,
+  copy_id INT NOT NULL,
+  branch_name VARCHAR(30) NOT NULL,
+  book_id INT NOT NULL,
+  PRIMARY KEY (copy_id),
+  FOREIGN KEY (branch_name) REFERENCES Branch(branch_name),
+  FOREIGN KEY (book_id) REFERENCES book(book_id)
+);
+
+CREATE TABLE Order_book
+(
+  order_status INT NOT NULL,
+  date_of_approve INT NOT NULL,
+  reader_email VARCHAR(50) NOT NULL,
+  copy_id INT NOT NULL,
+  PRIMARY KEY (reader_email, copy_id, date_of_approve),
+  FOREIGN KEY (reader_email) REFERENCES Reader(reader_email),
+  FOREIGN KEY (copy_id) REFERENCES copies(copy_id)
+);
