5cd659183509e68a54dbc33b19ad16605a44cdcf
ofirbrand <ofirbrand@gmail.com>
2023-02-10 13:40:40 +0200

    DB Host, DB Schema, MySQL username

--- ProjectApp/__init__.py
+++ ProjectApp/__init__.py
@@ -6,10 +6,10 @@ from flask_session import Session
 app = Flask(__name__)
 mysql = MySQL()
 app.config['SECRET_KEY'] = '5479437c16e750c4c8e3a432fe891395'
-app.config['MYSQL_DATABASE_HOST'] = '34.165.221.110'
-app.config['MYSQL_DATABASE_USER'] = 'group28'
-app.config['MYSQL_DATABASE_PASSWORD'] = '6jyaw40q'
-app.config['MYSQL_DATABASE_DB'] = 'group28_db'
+app.config['MYSQL_DATABASE_HOST'] = 'DB_Host'  #
+app.config['MYSQL_DATABASE_USER'] = 'DataBase_UserName'  # Insert your username
+app.config['MYSQL_DATABASE_PASSWORD'] = 'password'  # Insert your SQL password
+app.config['MYSQL_DATABASE_DB'] = 'DataBase_SchemaName'  # Schema date
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
